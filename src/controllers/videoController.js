21ab641d596e5f4fb60c17d2eed191285dad038a
bassoon1011 <bassoon1011@gmail.com>
2023-10-12 22:51:21 +1000

    Wetube Day-14 Study

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,12 +1,38 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+import Video from "../models/Video";
 
-export const see = (req, res) => res.render("watch");
+const handleSearch = (error, videos) => {
 
-export const edit = (req, res) => res.render("edit");
-
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const removeVideo = (req, res) => {
-    return res.send("Remove Video");
 }
 
+export const home = async (req, res) => {
+        const videos = await Video.find({});
+        return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+    const { id } = req.params;
+    return res.render("watch", { pageTitle: `Watching` }); 
+};
+export const getEdit = (req, res) => {
+    const { id } = req.params;
+    return res.render("edit", { pageTitle: `Editting` });
+};
+
+/** req.body는 edit.pug에있는 form 안에있는 value의 javascript representation이다.
+ * 이걸로 유저가 input에 작성한 data를 얻을 수 있다.(input에 name 줘야함) 
+ * 근데 이걸 가능하게 하려면 server에 urlencoded라는 middleware를 작성해야함.
+*/
+export const postEdit = (req, res) => {
+    const { id } = req.params;
+    const { title } = req.body;
+    return res.redirect(`/videos/${id}`);
+};
+
+export const getUpload = (req, res) => {
+    return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+/** upload.pug에 name으로 title을 줬기때문에 req.body로 그 이름의 데이터를 받을수 있음 */
+export const postUpload = (req, res) => {
+    const { title } = req.body;
+    return res.redirect("/");
+}; 
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,12 +1,21 @@
 import express from "express";
-import {see, edit, upload, removeVideo } from "../controllers/videoController";
+import {
+    watch, 
+    getUpload, 
+    getEdit, 
+    postEdit,
+    postUpload,
+} from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
-videoRouter.get("/:id", see);
-videoRouter.get("/:id/edit", edit);
-videoRouter.get("/:id/remove", removeVideo);
+/** /:id(\\d+ 이게 params임 */
+videoRouter.get("/:id(\\d+)", watch);
+// .post가 post request를 다뤄줌
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
+// videoRouter.get("/upload", getUpload); 
+// videoRouter.post("/upload", postUpload); 
+// 위에꺼 2개를 줄인게 밑에줄 
+videoRouter.route("/upload").get(getUpload).post(postUpload);
 
-
-export default videoRouter;
+export default videoRouter; 

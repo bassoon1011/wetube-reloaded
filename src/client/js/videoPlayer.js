c51c4d22c6faccd819b0a2f061f712d6dcd1543c
bassoon1011 <bassoon1011@gmail.com>
2023-11-20 22:46:52 +1000

    11.1 Bug- not fixed yet

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,9 +4,10 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volume = document.getElementById("volume");
 
-const handlePlay = (e) => {
+const handlePlayClick = (e) => {
     // if the video is playing,pause it
     if (video.paused) {
+        playBtn.innerText = "Pause";
         video.play();
     } else {
         video.pause();
@@ -14,9 +15,13 @@ const handlePlay = (e) => {
     // else play the video
 };
 
-const handleMute = (e) => {
-    a
-}
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "pause");
 
-playBtn.addEventListener("click", handlePlay);
+
+const handleMute = (e) => {}
+
+playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("Play", handlePlay);

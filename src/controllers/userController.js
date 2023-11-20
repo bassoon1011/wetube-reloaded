ec8046867f1da2a7c876da55ec9b366b7f08b56e
bassoon1011 <bassoon1011@gmail.com>
2023-11-20 21:13:52 +1000

    Revert "Update userController.js"
    This reverts commit 0d735d1ee8fcebaecf1771d6da520e55a38bf228.

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -50,14 +50,14 @@ export const postLogin = async (req, res) => {
     if (!user) {
         return res.status(400).render("login", {
             pageTitle,
-            errorMessage: "Username and/or password fail",
+            errorMessage: "An account with this username does not exists.",
         });
     }
     const ok = await bcrypt.compare(password, user.password);
     if (!ok) {
         return res.status(400).render("login", {
             pageTitle,
-            errorMessage: "Username and/or password fail",
+            errorMessage: "Wrong password",
         });
     }
     req.session.loggedIn = true;

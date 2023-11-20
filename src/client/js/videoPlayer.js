f7a06b69fb65edbde104f54f39122bc932a47130
bassoon1011 <bassoon1011@gmail.com>
2023-11-20 20:43:10 +1000

    Progress Save

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,22 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlay = (e) => {
+    // if the video is playing,pause it
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+    // else play the video
+};
+
+const handleMute = (e) => {
+    a
+}
+
+playBtn.addEventListener("click", handlePlay);
+muteBtn.addEventListener("click", handleMute);

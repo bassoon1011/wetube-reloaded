b02ac5d9657cd4e8eee2237198ff2fbdb7894e9e
Jaewon Park <bassoon1011@gmail.com>
2023-11-29 19:33:53 +1000

    ~11.5 bug not fixed

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,8 +1,12 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
-const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currenTime = documtnet.getElementById("currenTime");
+const totalTime = documtnet.getElementById("totalTime");
+
+let volumeValue = 0.5;
+video.volume = volumeValue;
 
 const handlePlayClick = (e) => {
     if (video.paused) {
@@ -21,8 +25,31 @@ const handleMute = (e) => {
         video.muted = true;
     }
     muteBtn.innerText = video.muted ? "Unmute" : "Mute";
-    volumeRange.value = video.muted ? 0 : 0.5;
+    volumeRange.value = video.muted ? 0 : volumeValue;
+};
+
+const handleVolumeChange = (event) => {
+    const {target: { value } } =event;
+    if(video.muted){
+        video.muted = false;
+        muteBtn.innerText = "Mute";
+    }
+    volumeValue = value;
+    video.volume = value;
+};
+
+const formatTime = (seconds) => new Date(seconds * 1000).toISoString().substr(11, 8);
+
+const handleLoadedMetadata = () => {
+    totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+
+const handleTimeUpdate = () => {
+    currenTime.innerText = formatTime(Math.floor(video.currentTime));
 };
 
 playBtn.addEventListener("click", handlePlayClick);
 video.addEventListener("Play", handlePlay);
+volumeRange.addEventListener("input", handleVolumeChange);
+video.addEventListener("loadedmetadata", handleLoadedMetadata);
+video.addEventListener("timeupdate", handleTimeUpdate);
